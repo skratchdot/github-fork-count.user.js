d7ab5173f394df56311a878ee4d4c400ffb62f97
skratchdot <git@skratchdot.com>
2012-07-14 14:14:28 -0400

    updating to show more useful info.

--- github-fork-count.user.js
+++ github-fork-count.user.js
@@ -1,49 +1,70 @@
 // ==UserScript==
 // @name           Github Fork Count
 // @namespace      https://github.com/skratchdot/github-fork-count.user.js
-// @description    A user script to display the fork count underneath the "public" repo count on a user's main GitHub page.
+// @description    A user script to display repo counts (public, private, sources, forks, mirrors) where the "public" repo counts used to be.
 // @include        https://github.com/*
 // @match          https://github.com/*
 // @run-at         document-end
 // @icon           http://skratchdot.com/favicon.ico
 // @downloadURL    https://github.com/skratchdot/github-fork-count.user.js/raw/master/github-fork-count.user.js
 // @updateURL      https://github.com/skratchdot/github-fork-count.user.js/raw/master/github-fork-count.user.js
-// @version        1.0
+// @version        1.1
 // ==/UserScript==
 /*global jQuery */
 /*jslint browser: true */
 
-var main = function () {
+var userScript = function () {
 	'use strict';
 
 	jQuery(document).ready(function () {
 		// Initial our variables (and jQuery selectors)
-		var forkCount = 0,
-			repoList = jQuery('ul.repo_list li'),
-			publicRepos = jQuery('div.site div.columns div.last ul.stats li:first a:first');
+		var countRepos = 0,
+			countPublic = 0,
+			countPrivate = 0,
+			countSources = 0,
+			countForks = 0,
+			countMirrors = 0,
+			repoList = jQuery('ul.repo_list > li'),
+			stats = jQuery('body.page-profile div.profilecols ul.stats');
 
 		// Loop through all repos, looking for public forks
 		repoList.each(function () {
 			try {
 				var elem = jQuery(this);
-				if (elem.hasClass('public') && elem.hasClass('fork')) {
-					forkCount = forkCount + 1;
+				countRepos = countRepos + 1;
+				if (elem.hasClass('public')) {
+					countPublic = countPublic + 1;
+				}
+				if (elem.hasClass('private')) {
+					countPrivate = countPrivate + 1;
+				}
+				if (elem.hasClass('source')) {
+					countSources = countSources + 1;
+				}
+				if (elem.hasClass('fork')) {
+					countForks = countForks + 1;
+				}
+				if (elem.hasClass('mirror')) {
+					countMirrors = countMirrors + 1;
 				}
 			} catch (e) {}
 		});
 
 		// Display Fork Count (profile page - right column)
-		if (publicRepos.length > 0) {
-			publicRepos.append('<span style="margin-top:-2px;">' +
-				forkCount + ' fork' +
-				(forkCount === 1 ? '' : 's') +
-				'</span>'
-				);
+		if (stats.length > 0) {
+			stats.append('<li>' +
+				'<strong>' + countRepos + '<span style="display:inline-block;font-weight:normal;">&nbsp;repos</span></strong>' +
+				'<span>' + countPublic + ' public, ' +
+				countPrivate + ' private</span>' +
+				'<span style="margin:0">' + countSources + ' sources, ' +
+				countForks + ' forks</span>' +
+				(countMirrors > 0 ? '<span style="margin:0">' + countMirrors + ' mirrors</span>' : '') +
+				'</li>');
 		}
 	});
 };
 
-// Inject our main script
+// Inject our script onto the page
 var script = document.createElement('script');
-script.textContent = '(' + main.toString() + ')();';
+script.textContent = '(' + userScript.toString() + ')();';
 document.body.appendChild(script);
